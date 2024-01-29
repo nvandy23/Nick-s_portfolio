9263e2671678b37c36ab91f2c7803cb07e296350
nvandy23 <nvandy2373@gmail.com>
2024-01-29 14:31:08 -0500

    setup basic footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
 function Footer(props) {
     return (
-      <h1>Source:</h1>
+      <div> 
+      <h1> Richard (Nick) Vanderipe 2024 All rights reserved</h1>
+      <h2> Site powered by: <img src=".././src/assets/Vitejs-logo.svg.png"></img> <img src=".././src/assets/react-1-logo-png.png"></img> </h2>
+      </div>
     )
 
   }
